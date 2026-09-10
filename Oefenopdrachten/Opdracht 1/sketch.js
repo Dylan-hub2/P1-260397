d20bb06c68d6b6dda82797bf47f8783e50850fbc
Dylan-hub2 <dylan@vanmarkus.nl>
2026-09-10 09:09:50 +0200

    net zo goed alles van de basis opdrachten toegevoegd
    ik moet nog alleen de extra uitdagingen doen

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,42 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
 }
 
 function draw() {
-  background(220);
+
+  strokeWeight(2.5)
+
+  background(125);
+  fill("#fc0606");
+  rect(100,100,175,100);
+fill("#013cff");
+  rect(1,1,75,75);
+
+fill("#ffffff");
+rect(0,175,100,25);
+
+fill("#ffffff");
+rect(0,100,25,75);
+
+fill("#ffffff");
+rect(25,100,75,75);
+
+rect(0,75,100,25);
+
+rect(100,75,100,25);
+
+rect(150,25,50,50);
+
+rect(75,25,25,75);
+
+fill("#ffff00");
+rect(75,0,125,25);
+
+fill("#0037ff");
+rect(100,25,50,50);
+
+fill("#000000");
+rect(25,125,75,1);
+
+rect(0,175,25,50)
 }
--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,8 +1,299 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(3000, 1800);
 }
 
 function draw() {
-  background(220);
-  circle(300, 360, 50)
+  background(150);
+  fill(0);
+    rect(237,250,150,350);
+
+  fill(255,0,0);
+  circle(310, 310, 100)
+
+  fill("#f3a113ed")
+  circle(310,420,100)
+
+  fill("#2fff00")
+  circle(310,530,100)
+
+  fill(0)
+  rect(262.5,600,100,250)
+
+  text("5.stoplicht",250,220)
+
+  text("1.Dylan van Markus",50,50)
+
+  fill("#ff0000ca")
+  rect(100,100,150,50)
+
+  fill(255)
+  rect(100,150,150,50)
+
+  fill("#030bfa")
+  rect(100,200,150,50)
+fill(0)
+  text("2.flag",95,95)
+
+  rect(800,800,100,100)
+
+  rect(600,800,100,100)
+
+  rect(700,700,100,100)
+
+  rect(600,600,100,100)
+
+  rect(800,600,100,100)
+
+  fill(255)
+  rect(700,800,100,100)
+
+  rect(700,600,100,100)
+
+  rect(800,700,100,100)
+
+  rect(600,700,100,100)
+
+  fill(0)
+  text("3.schaakbord",550,550)
+// ik maak de fill hetzelfde als de background zodat alleen de lijn van strokeweight zichtbaar blijft
+  strokeWeight(1)
+  fill(150)
+  triangle(1000,1350,1150,1200,1300,1350)
+
+  rect(1000,1350,300,250)
+
+
+  fill(0)
+  text("4.transparant huisje",1200,1200)
+
+  strokeWeight(5)
+  fill(255)
+  rect(250,1250,250,250,25)
+
+  fill(0)
+  circle(373,1375,75)
+
+  text("6.dobbelsteen",200,1240)
+
+  //ik heb na dit alleen mario en een goomba gemaakt
+  strokeWeight(0)
+
+  fill("#ff0202")
+  rect(875,25,125,25)
+  
+  fill("#ff0202")
+  rect(850,50,225,25)
+
+  fill("#f8ac72")
+  rect(850,75,150,25)
+
+  fill("#f8ac72")
+  rect(825,100,250,25)
+  
+  fill("#f8ac72")
+  rect(825,125,275,25)
+
+  fill("#f8ac72")
+  rect(825,150,250,25)
+
+  fill("#f8ac72")
+  rect(875,175,175,25)
+  
+  fill("#ff0000")
+  rect(850,200,175,25)
+  
+  fill("#ff0000")
+  rect(825,225,250,25)
+  
+  fill("#ff0000")
+  rect(800,250,300,25)
+
+fill("#f8ac72")
+  rect(800,275,300,25)
+
+  fill("#ff0000")
+  rect(850,275,200,25)
+
+  fill("#0d00ff")
+  rect(875,275,150,25)
+
+  fill("#fff700")
+  rect(900,275,25,25)
+
+  fill("#fff700")
+  rect(975,275,25,25)
+
+  fill("#f8ac72")
+  rect(800,300,300,25)
+
+  fill("#0d00ff")
+  rect(875,300,150,25)
+
+  fill("#f8ac72")
+  rect(800,325,300,25)
+
+  fill("#0d00ff")
+  rect(850,325,200,25)
+
+  fill("#0d00ff")
+  rect(850,350,75,25)
+
+  fill("#0d00ff")
+  rect(975,350,75,25)
+
+  fill("#ac4d08c6")
+  rect(1000,375,100,25)
+
+  fill("#ac4d08c6")
+  rect(800,375,100,25)
+
+  fill("#ac4d08c6")
+  rect(775,400,125,25)
+ 
+  fill("#ac4d08c6")
+  rect(1000,400,125,25)
+
+   fill("#0d00ff")
+  rect(900,250,100,25)
+
+  fill("#0d00ff")
+  rect(900,225,25,25)
+
+  fill("#0d00ff")
+  rect(975,225,25,25)
+
+  fill("#0d00ff")
+  rect(900,200,25,25)
+
+  fill("#593905")
+  rect(850,75,75,25)
+
+  fill("#0a0500")
+  rect(975,75,25,25)
+
+  fill("#593905")
+  rect(825,100,25,25)
+
+  fill("#593905")
+  rect(875,100,25,25)
+
+  fill("#0a0500")
+  rect(975,100,25,25)
+
+  fill("#593905")
+  rect(825,125,25,25)
+
+  fill("#593905")
+  rect(875,125,50,25)
+
+  fill("#0a0500")
+  rect(1000,125,25,25)
+
+  fill("#593905")
+  rect(825,150,50,25)
+
+  fill("#0a0500")
+  rect(975,150,100,25)
+
+  text("7.Mario",1000,20)
+
+  fill("#753518df")
+  rect(2000,300,75,25)
+
+  
+  fill("#753518df")
+  rect(1975,325,125,25)
+
+fill("#753518df")
+  rect(1950,350,175,25)
+
+  fill("#753518df")
+  rect(1925,375,225,25)
+
+  fill("#753518df")
+  rect(1900,400,275,25)
+
+  fill("#070200df")
+  rect(1925,400,50,25)
+
+  fill("#070200df")
+  rect(2100,400,50,25)
+
+  fill("#753518df")
+  rect(1875,425,325,25)
+  
+  fill("#753518df")
+  rect(1875,450,325,25)
+
+  fill("#753518df")
+  rect(1850,475,375,25)
+  
+  fill("#753518df")
+  rect(1850,500,375,25)
+
+  fill("#753518df")
+  rect(1850,525,375,25)
+
+fill("#753518df")
+  rect(1875,550,325,25)
+
+fill("#dabcaf")
+  rect(1925,575,225,25)
+
+fill("#070200df")
+  rect(1875,600,325,25)
+
+fill("#070200df")
+  rect(1850,625,375,25)
+
+  fill("#070200df")
+  rect(1850,650,375,25)
+
+  fill("#070200df")
+  rect(1875,675,325,25)
+
+  fill("#000000")
+  rect(1975,425,25,75)
+  
+  fill("#000000")
+  rect(2075,425,25,75)
+  
+    fill("#ffffff")
+  rect(1950,425,25,75)
+
+  fill("#ffffff")
+  rect(2100,425,25,75)
+
+  fill("#0b0b0bdf")
+  rect(1975,450,100,25)
+
+  fill("#ffffff")
+  rect(2000,475,25,25)
+
+  fill("#ffffff")
+  rect(2050,475,25,25)
+
+  fill("#ffffff")
+  rect(1950,500,75,25)
+
+  fill("#ffffff")
+  rect(2050,500,75,25)
+
+  fill("#dabcaf")
+  rect(1950,550,175,25)
+
+  fill("#dabcaf")
+  rect(1925,600,225,25)
+
+  fill("#dabcaf")
+  rect(1975,625,125,25)
+
+  fill("#dabcaf")
+  rect(2012.5,650,50,50)
+
+  fill(0)
+  text("8.Goomba",2050,275)
+
+  
 }
+
