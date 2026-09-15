781589a70a15a8fbea4324a1dc1ede925c87564b
Dylan-hub2 <dylan@vanmarkus.nl>
2026-09-15 11:50:13 +0200

    tyfus

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
 function draw() {
   background(220);
+  
+fill(0)
+  if (score > 90) {
+  text("Uitstekend!", 100, 100);
+} else if (score > 70) {
+  text("Goed!", 100, 100);
+} else if (score > 50) {
+  text("Voldoende", 100, 100);
+} else {
+  text("Onvoldoende", 100, 100);
+}
+
 }
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -5,14 +5,19 @@ function setup() {
 function draw() {
   background(255);
   strokeWeight(0)
-
-  fill("#42b3ff")
+// ik ga eerst de dingen zonder variablelelele doen
+  fill("#99eafe")
   rect(0,0,800, 600)
 
-  fill("#888080")
-  triangle(250,50,25,450,500,450)
+  fill("#524d4d")
+  triangle(250,50,25,500,500,500)
+fill("#023800")
+  rect(0,500,800,150)
 
-  fill("#888080")
-  triangle(500,300,250,450,100,450)
+  fill("#4e534e")
+  rect(0,505,800,150)
 
+  fill(0)
+rect(700,400,10,100)  
+ 
 }
