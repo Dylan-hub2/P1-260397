cea5114293d2abc681909660add9cf5b5d2626b9
Dylan-hub2 <dylan@vanmarkus.nl>
2026-09-15 10:20:54 +0200

    berg toegevoegd

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,9 +1,18 @@
 function setup() {
-  createCanvas(1600, 1200);
+  createCanvas(800, 600);
 }
 
 function draw() {
-  background(220);
-  
+  background(255);
+  strokeWeight(0)
+
+  fill("#42b3ff")
+  rect(0,0,800, 600)
+
+  fill("#888080")
+  triangle(250,50,25,450,500,450)
+
+  fill("#888080")
+  triangle(500,300,250,450,100,450)
 
 }
